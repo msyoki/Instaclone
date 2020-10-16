e4056fcf00a1c2202a05b58675ce17ef7f5dd7dc
msyoki <msyokimutua@gmail.com>
2020-10-16 22:05:56 +0300

    add views function for  image object render

--- Instagram/Instaclone/views.py
+++ Instagram/Instaclone/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Image
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')  
 def home(request):
-    return render(request,'Instaclone/index.html')
+    images = Image.get_all_images()
+    return render(request,'Instaclone/index.html',{"images":images})
