5f2f63560ea610556fdf2571a6edb0ee2f6a09ed
msyoki <msyokimutua@gmail.com>
2020-10-17 16:33:54 +0300

    update urls for image post

--- Instagram/Instaclone/urls.py
+++ Instagram/Instaclone/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.home,name='home')
+    url(r'^$',views.home,name='home'),
+    url(r'^new/image$', views.new_image, name='new-image'),
+    url(r'^new/post$',views.home, name='newpost'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
