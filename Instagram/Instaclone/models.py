7607ffa7e611dfb2ee34c9de68371c26d3a8e4e7
msyoki <msyokimutua@gmail.com>
2020-10-18 23:01:03 +0300

    update models

--- Instagram/Instaclone/models.py
+++ Instagram/Instaclone/models.py
@@ -3,22 +3,38 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Image(models.Model):
-    image=models.ImageField(upload_to='photos',null=True)
-    image_name=models.CharField(max_length=20)
-    image_caption=models.CharField(max_length=50)
-    like=models.IntegerField(default=0)
-    profile=models.ForeignKey(User,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='photos')
+    name=models.CharField(max_length=20)
+    caption=models.CharField(max_length=50)
+    like=models.IntegerField(null=True)
+    username=models.ForeignKey(User,on_delete=models.CASCADE)
+    post_on = models.DateTimeField(auto_now_add= True,null=True)
+
+
+    def __str__(self):
+        return self.username
 
 
 class Profile(models.Model):
-    profile_pic = models.ImageField(upload_to= 'profile_pic/', null= True, blank= True)
-    first_name = models.CharField(max_length =30,blank=True)
-    last_name = models.CharField(max_length =30,blank=True)
+    profile_pic = models.ImageField(upload_to= 'profile_pic/')
+    name = models.CharField(max_length=150,null=True)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    email= models.EmailField(null=True)
     bio = models.TextField(max_length = 50, blank= True)
+        
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def search_profile(cls,search_term):
+        profiles = cls.objects.filter(Q(username__username=search_term) | Q(name__icontains=search_term))
+
+        return profiles
+
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    username = models.ForeignKey(User,on_delete = models.CASCADE)
     Image = models.ForeignKey(Image,on_delete = models.CASCADE)
     comment = models.CharField(max_length= 70)
     post_on = models.DateTimeField(auto_now_add= True)
