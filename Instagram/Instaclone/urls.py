ec246b5d335d61b427fe9edd6ef6269d003121d7
msyoki <msyokimutua@gmail.com>
2020-10-18 09:53:05 +0300

    add search url

--- Instagram/Instaclone/urls.py
+++ Instagram/Instaclone/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^new/image$', views.new_image, name='new-image'),
     url(r'^new/post$',views.home, name='newpost'),
+    url(r'^search/', views.search_results, name='search_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
