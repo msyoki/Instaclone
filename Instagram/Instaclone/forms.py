9714fa7e95dc4ef8c10ec23919ea5a24d109700c
msyoki <msyokimutua@gmail.com>
2020-10-18 16:59:52 +0300

    add profile form

--- Instagram/Instaclone/forms.py
+++ Instagram/Instaclone/forms.py
@@ -1,8 +1,13 @@
-from .models import Image
+from .models import Image,Profile
 from django import forms
 
 class ImagePostForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['like', 'post_on','user','profile']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['username']
   
